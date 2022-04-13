e539c4b2d7f7746ad1271daa84aeeb4049271ed4
Lonny Vberg <lonnyvandenberg@gmail.com>
2022-04-13 00:38:51 -0600

    added bcrypt and hashed password for post and put requests

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -56,6 +56,7 @@ router.get('/:id', (req, res) => {
 
         // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
         User.update(req.body, {
+            individualHooks: true,
             where: {
                 id: req.params.id
             }
