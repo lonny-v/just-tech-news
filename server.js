1ff6b601e28c4778edab0e410f191cda71d49ad2
Lonny Vberg <lonnyvandenberg@gmail.com>
2022-04-26 14:06:33 -0600

    added ability to see user's posts when logged in

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-const express = require('express');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
 const path = require('path');
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({ helpers });
+const express = require('express');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const helpers = require('./utils/helpers');
 
 const sess = {
     secret: 'Super secret secret',
@@ -18,22 +19,21 @@ const sess = {
     })
 };
 
-
-
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
 app.use(session(sess));
 
-// turn on routes
-app.use(routes);
+const helpers = require('./utils/helpers');
 
-// turn on connection to db and server
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'));
+
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
